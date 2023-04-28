7265fe627a31edbe14fcae75e45680e69abbdd91
Valery <pro6645@yandex.ru>
2023-04-28 21:47:44 +0300

    Начало работы с регистрацией

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
+from flask import Flask, render_template, redirect
+from data import db_session
 from town import get_random_town, do_map_file
 from data.get_object import GetTownForm
+from users import User, LoginForm
 
 
 app = Flask(__name__)
@@ -52,6 +54,21 @@ def game_town():
         do_map_file(bot_town)
         return render_template('map.html', **param, form=form)
     return render_template('map.html', **param, form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
