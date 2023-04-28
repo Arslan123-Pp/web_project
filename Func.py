4a9519197babfe59f9b43d889f9c807aff35317d
Valery <pro6645@yandex.ru>
2023-04-28 20:42:40 +0300

    Добавлен список остальных городов мира городов

--- Func.py
+++ Func.py
@@ -2,21 +2,26 @@ import random
 
 
 def func(a):
-    f = open("Town.txt", encoding='utf-8')
+    t = random.choice([1, 2])
+    if t == 1:
+        f = open("Town.txt", encoding='utf-8')
+    elif t == 2:
+        f = open("Town_2.txt", encoding='utf-8')
     s = f.readlines()
     s = [x.strip() for x in s]
-    d = []
+    dic = []
     for i in s:
         if i.lower()[0] == a:
-            d.append(i)
-    return random.choice(d)
+            dic.append(i)
+    return random.choice(dic)
 
 # while True:
-#     print(func(input()))
-    # v = func('б')
-    # print(v)
-    # if v == 'Бугульма':
-    #     break
-    # v = func('а')
-    # if v == 'Азнакаево':
-    #     break
+#     # print(func(input()))
+#     v = func('б')
+#     print(v)
+#     if v == 'Бугульма':
+#         break
+#     v = func('а')
+#     print(v)
+#     if v == 'Азнакаево':
+#         break
